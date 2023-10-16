69f0d208756278fe9174047384fa06acda3cb2ad
lm1618 <63717679+lm1618@users.noreply.github.com>
2023-10-16 09:46:00 +0800

    Add files via upload

---
+++ database/tsvok.py
@@ -0,0 +1,29 @@
+#将TSV转换为pickle，TSV下载地址http://addresses.loyce.club/
+#此版本为Linux版本，windows修改路径地址即可
+# coding=UTF-8
+import csv
+import pickle
+import os
+filename="blockchair_bitcoin.tsv"
+#初始化，创建空set
+s=set()  
+count=0
+
+num=0
+with open(os.getcwd()+"/"+filename) as f:
+     data=csv.reader(f,delimiter='\t')
+     for line in data:
+        # print(line[0])
+        count=count+1
+        s.add(line[0])
+        if count%1000000==0:
+            print(s)
+            with open(os.getcwd()+"/"+str(num).zfill(3)+".pickle","wb") as pkl:
+                pickle.dump(s,pkl)
+                s.clear()
+            num=num+1
+     num=num+1
+     with open(os.getcwd()+"/"+str(num).zfill(3)+".pickle","wb") as pkl:
+        pickle.dump(s,pkl)
+        s.clear()
+            
