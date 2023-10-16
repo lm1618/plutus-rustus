e2e95eb7a5df03f4e12ad08c9f546cc3107ad6e0
lm1618 <63717679+lm1618@users.noreply.github.com>
2023-10-16 10:42:56 +0800

    Update tsvok.py

--- database/tsvok.py
+++ database/tsvok.py
@@ -1,12 +1,13 @@
 #将TSV转换为pickle，TSV下载地址http://addresses.loyce.club/
 #此版本为Linux版本，windows修改路径地址即可
+
 # coding=UTF-8
 import csv
 import pickle
 import os
 filename="blockchair_bitcoin.tsv"
-#初始化，创建空set
-s=set()  
+#创建空List
+s=[]
 count=0
 
 num=0
@@ -15,8 +16,8 @@ with open(os.getcwd()+"/"+filename) as f:
      for line in data:
         # print(line[0])
         count=count+1
-        s.add(line[0])
-        if count%1000000==0:
+        s.append(line[0])
+        if count%500000==0:
             print(s)
             with open(os.getcwd()+"/"+str(num).zfill(3)+".pickle","wb") as pkl:
                 pickle.dump(s,pkl)
